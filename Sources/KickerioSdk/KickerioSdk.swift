41933433887f507432d2db1c004a3611c14454f1
Robin Goos <robingoos@gmail.com>
2021-04-07 13:45:58 +0200

    add convenience initializer to sdk

--- Sources/KickerioSdk/KickerioSdk.swift
+++ Sources/KickerioSdk/KickerioSdk.swift
@@ -3,8 +3,38 @@ import Foundation
 
 struct KickerioSdk {
     private static let baseURL = "http://localhost:3000"
+    private static let apiKeyPlistKey = "kickerio_api_key"
+    private static let appNamePlistKey = "CFBundleName"
+    private static let appVersionPlistKey = "CFBundleShortVersionString"
+    private static let buildNumberPlistKey = "CFBundleVersion"
 
-    func checkApplicationDeprecation(apiKey: String, appName: String, appVersion: String, buildNumber: String, platformVersion: String, parameters: [String: String], onComplete: @escaping (DataResponse<KickerioResponse, AFError>) -> ()) {
+    private let apiKey: String
+    private let appName: String
+    private let appVersion: String
+    private let buildNumber: String
+    private let platformVersion: String
+    private let parameters: [String: String]
+
+    convenience init(bundle: Bundle = Bundle.main,
+                     processInfo: ProcessInfo = ProcessInfo.processInfo,
+                     parameters: [String: String] = [:]) throws {
+        let apiKey: String = try bundle.value(forKey: Self.apiKeyPlistKey)
+        let appName: String = try bundle.value(forKey: Self.appNamePlistKey)
+        let appVersion: String = try bundle.value(forKey: Self.appVersionPlistKey)
+        let buildNumber: String = try bundle.value(forKey: Self.buildNumberPlistKey)
+        let platformVersion: String = processInfo.operatingSystemVersionString
+    }
+
+    init(apiKey: String,
+         appName: String,
+         appVersion: String,
+         buildNumber: String,
+         platformVersion: String,
+         parameters: [String: String]) {
+
+    }
+
+    func checkApplicationDeprecation(onComplete: @escaping (DataResponse<KickerioResponse, AFError>) -> ()) {
         let headers: HTTPHeaders = [
             "Accept": "application/json",
             "X-API-KEY": apiKey,
@@ -28,5 +58,22 @@ struct KickerioSdk {
         AF.request(Self.baseURL + "/api/v1/target-checks", method: .post, parameters: parameters, headers: headers).responseDecodable(of: KickerioResponse.self, decoder: decoder) { response in
                 onComplete(response as DataResponse<KickerioResponse, AFError>)
         }
+    }
+}
+
+extension Bundle {
+    enum Error: Swift.Error {
+        case noValueFound
+        case badType
+    }
+
+    func value<T>(forKey key: String) throws -> T {
+        guard let value = object(forInfoDictionaryKey: key) else {
+            throw Error.noValueFound
+        }
+        guard let coercedValue = value as? T else {
+            throw Error.badType
+        }
+        return coercedValue
     }
 }
